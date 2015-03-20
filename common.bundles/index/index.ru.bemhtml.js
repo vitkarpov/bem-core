b19cea37e613abc5faefc724d36a5dd1d1d5b879
aristov <vv.aristov@gmail.com>
2015-03-20 17:29:46 +0300

    add samples for techs development

---
+++ common.bundles/index/index.ru.bemhtml.js
@@ -0,0 +1,16 @@
+/* borschik:include:index.bemhtml.js */
+
+oninit(function(exports, context) {
+
+(context.BEMContext.prototype.i18n = (/* borschik:include:../../common.blocks/i18n/i18n.i18n.js */)()).decl();
+
+context.BEMContext.prototype.i18n.decl({
+    "b-square" : {
+        "js" : "ДжаваСкрипт"
+    },
+    "b-circle" : {
+        "css" : "Каскадные таблицы стилей"
+    }
+});
+
+});
---
+++ common.bundles/index/index.ru.i18n.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "b-square" : {
+        "js" : "ДжаваСкрипт",
+        "js2" : function(x) {
+            return 'asdf' + x;
+        }
+    },
+    "b-circle" : {
+        "css" : "Каскадные таблицы стилей"
+    }
+};
