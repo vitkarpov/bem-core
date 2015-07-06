ebdffefaf2edd72a151b56bec16f75d9aaa4cd89
aristov <vv.aristov@gmail.com>
2015-07-06 17:03:11 +0300

    wip

--- .enb/make.js
+++ .enb/make.js
@@ -10,21 +10,23 @@ config.node('common.bundles/i18n', function(nodeConfig) {
         require('enb-bem-techs/techs/deps'),
         require('enb-bem-techs/techs/files'),
 
-        [require('enb/techs/js'), { target : '?.pre.js', sourceSuffixes  : ['vanilla.js', 'js', 'browser.js'] }],
-        [require('enb-bem-i18n/techs/i18n-js'), { target : '?.i18n.{lang}.js', lang : '{lang}' }]
-    ]);
-    nodeConfig.addTechs(config.getLanguages().map(function(lang) {
-        return [require('enb/techs/file-merge'), {
-            sources : ['?.pre.js', '?.i18n.' + lang + '.js'],
-            target : '?.pre.' + lang + '.js' }];
-    }));
-    nodeConfig.addTechs([
-        [require('enb-modules/techs/prepend-modules'), { source : '?.pre.{lang}.js', target : '?.{lang}.js' }],
-
         [require('enb-bem-i18n/techs/keysets'), { lang : '{lang}' }],
 
+        [require('enb/techs/js'), { target : '?.pre.js', sourceSuffixes  : ['vanilla.js', 'js', 'browser.js'] }],
+        [require('enb-bem-i18n/techs/i18n-js'), { target : '?.i18n.{lang}.js', lang : '{lang}' }],
+        [require('enb/techs/file-merge'), {
+            lang : '{lang}',
+            sources : ['?.pre.js', '?.i18n.{lang}.js'],
+            target : '?.pre.{lang}.js'
+        }],
+        [require('enb-modules/techs/prepend-modules'), { source : '?.pre.{lang}.js', target : '?.{lang}.js' }],
+
         [require('enb-bem-i18n/techs/bemhtml-i18n'), { lang : '{lang}', devMode : true }],
-        [require('./techs/i18n-bemjson-to-html'), { templateFile : '?.bemhtml.{lang}.js', lang : '{lang}' }]
+        [require('enb-bemxjst/techs/html-from-bemjson'), {
+            bemhtmlTarget : '?.bemhtml.{lang}.js',
+            lang : '{lang}',
+            target : '?.{lang}.html'
+        }]
     ]);
 
     nodeConfig.addTargets([
